273964226c5d93f3f94404724d20897a067ec2ec
zerock <cookieKang@gmail.com>
2020-01-22 17:38:40 +0900

    Map Display

---
+++ app/App.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Geolocation from '@react-native-community/geolocation'
+
+
+const App = () => {
+
+    return ( 
+        <View>
+
+        </View>
+    )
+
+}
+
+export default App
+
